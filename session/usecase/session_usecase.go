2240b03e52e1fd7fa273519a9ad6553393b83482
bekzemed <bereketzemed@gmail.com>
2020-01-10 15:21:06 +0300

    implemented session repository and usecase

--- session/usecase/session_usecase.go
+++ session/usecase/session_usecase.go
@@ -13,26 +13,32 @@ func NewSessionUsecase(conn *gorm.DB) *SessionUsecase {
 	return &SessionUsecase{conn: conn}
 }
 
-func (s *SessionUsecase) Sessions() ([]models.Session, error) {
-	panic("implement me")
+func (s *SessionUsecase) Sessions() ([]models.Session, []error) {
+	data, err := s.Sessions()
+	return data, err
 }
 
-func (s *SessionUsecase) DeleteSession(id int) (models.Session, error) {
-	panic("implement me")
+func (s *SessionUsecase) DeleteSession(id int) (models.Session, []error) {
+	data, err := s.DeleteSession(id)
+	return data, err
 }
 
-func (s *SessionUsecase) UpdateSession(sess *models.Session) (*models.Session, error) {
-	panic("implement me")
+func (s *SessionUsecase) UpdateSession(sess *models.Session) (*models.Session, []error) {
+	data, err := s.UpdateSession(sess)
+	return data, err
 }
 
-func (s *SessionUsecase) StoreSession(sess *models.Session) (*models.Session, error) {
-	panic("implement me")
+func (s *SessionUsecase) StoreSession(sess *models.Session) (*models.Session, []error) {
+	data, err := s.StoreSession(sess)
+	return data, err
 }
 
-func (s *SessionUsecase) GetSession(value string) (models.Session, error) {
-	panic("implement me")
+func (s *SessionUsecase) GetSession(value string) (models.Session, []error) {
+	data, err := s.GetSession(value)
+	return data, err
 }
 
-func (s *SessionUsecase) Check(sess *models.Session) (models.Session, error) {
-	panic("implement me")
+func (s *SessionUsecase) Check(sess *models.Session) (models.Session, []error) {
+	data, err := s.Check(sess)
+	return data, err
 }
